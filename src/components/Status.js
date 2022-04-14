760eefe6134d2a3a2d5b69abf1b5cdebaf70b2f5
Ying <zy414371666@gmail.com>
2022-04-14 16:26:54 -0700

    added confetti animation for correct guess

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,8 +1,12 @@
 import {useSelector, useDispatch, shallowEqual} from "react-redux";
 import {disableInput} from '../actions';
 import {Alert} from "react-bootstrap";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Status.css';
+
+import Confetti from 'react-confetti'
+// import useWindowSize from './useWindowSize';
+
 
 
 export default function Status(props) {
@@ -11,25 +15,53 @@ export default function Status(props) {
     const dispatch = useDispatch();
 
     let message = "";
+    let { width, height } = useWindowSize();
 
     if (!guessResult && remainAttemptNumber > 0) {
         message = `You have ${remainAttemptNumber} more chance(s).`;
+        width = 0; height = 0;
     } else { // End of Game situations:
         if (guessResult) {
             message = "Congrats! You've got the correct word!";
         } else {
             message = "Game Over! You've run out of chances!";
+            width = 0; height = 0;
         }
 
         dispatch(disableInput());
     }
 
     return (
-        // <div>
-        //     Status: {message}
-        // </div>
-        <Alert className="alert" variant="warning" style={{padding: "0.5rem 0.5rem"}}>
-            <p style={{marginBottom: "0"}}>{message}</p>
-        </Alert>
+        <div>
+            <Alert className="alert" variant="warning" style={{padding: "0.5rem 0.5rem"}}>
+                <p style={{marginBottom: "0"}}>{message}</p>
+            </Alert>
+            <Confetti width={width} height={height}/>
+        }
+        </div>
     )
+}
+//function to get the window size of the device
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
 }
